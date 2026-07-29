45c62820947d36e81bf4be3de7aacadb4356eddc
robin <hossainmdrobin967@gmail.com>
2026-07-29 13:53:21 +0600

    sdf

--- src/app/app/dashboard/members/[id]/page.tsx
+++ src/app/app/dashboard/members/[id]/page.tsx
@@ -1,10 +1,7 @@
-import ProtectedRoute from "@/components/ProtectedRoute";
 import MemberDetails from "@/views/MemberDetails";
 
 export default function Page() {
   return (
-    <ProtectedRoute>
       <MemberDetails />
-    </ProtectedRoute>
   );
 }
--- src/components/members/[id].tsx/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function MemberDetail() {
-  return (
-    <div>page</div>
-  )
-}
