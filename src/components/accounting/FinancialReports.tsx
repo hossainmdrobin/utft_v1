2c057ffec27711bc5503dfe48ab427d6e1aee33e
robin <hossainmdrobin967@gmail.com>
2026-08-11 02:01:56 +0600

    sdf

--- src/components/accounting/FinancialReports.tsx
+++ src/components/accounting/FinancialReports.tsx
@@ -1,5 +1,3 @@
-import { useQuery } from "@tanstack/react-query";
-import { supabase } from "@/integrations/mongodb/client";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import {
